a675ff4c53c61df54659b6c2c86c62d3353db97c
karuna24s <karuna24s@gmail.com>
2016-10-11 14:14:36 -0400

    Add initial User model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with an email" do
+    user = User.new(email: 'test@example.com', password: 'password')
+    expect(user).to be_valid
+  end
+  it "is invalid without an email address" do
+    user = User.new(email: nil, password: 'password')
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+  it "is invalid with a duplicate email address" do
+    User.create(email: 'test@example.com', password: 'password')
+    user = User.new(email: 'test@example.com', password: 'password')
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
 end
