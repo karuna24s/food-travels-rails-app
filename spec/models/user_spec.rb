5cbfaef4cf6099a9ae2073437b78d824cc136452
karuna24s <karuna24s@gmail.com>
2016-10-11 23:54:41 -0400

    Update User model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with an email" do
-    user = User.new(email: 'test@example.com', password: 'password')
-    expect(user).to be_valid
+  before :each do
+    @user = User.create(
+      name: 'Jackie',
+      email: 'jackie@example.com',
+      password: 'password'
+      )
   end
-  it "is invalid without an email address" do
-    user = User.new(email: nil, password: 'password')
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
+
+  describe "information is valid" do
+    it "is valid with a name and email" do
+      expect(@user).to be_valid
+    end
   end
-  it "is invalid with a duplicate email address" do
-    User.create(email: 'test@example.com', password: 'password')
-    user = User.new(email: 'test@example.com', password: 'password')
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
+
+  describe "information cannot be left blank" do
+    it "is invalid without a name" do
+      user = User.new(name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+    it "is invalid without an email address" do
+      user = User.new(email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
   end
+
+  describe "information cannot already be in use" do
+    it "is invalid with a duplicate email address" do
+      User.create(name: 'test', email: 'test@example.com', password: 'password')
+      user = User.new(name: 'test', email: 'test@example.com', password: 'password')
+      user.valid?
+      expect(user.errors[:email]).to include("has already been taken")
+    end
+  end
+
 end
