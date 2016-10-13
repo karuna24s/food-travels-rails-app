0d588fc33708c4dfec3d218acd6f64bdfd00e1e0
karuna24s <karuna24s@gmail.com>
2016-10-13 05:57:22 -0400

    Implement factory user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before :each do
-    @user = User.create(
-      name: 'Jackie',
-      email: 'jackie@example.com',
-      password: 'password'
-      )
-  end
 
-  describe "information is valid" do
+  describe "misc" do
+    before (:each) do
+      @user = create(:user)
+    end
+
+    it "has a valid factory" do
+      expect(@user).to be_valid
+    end
+
     it "is valid with a name and email" do
       expect(@user).to be_valid
     end
   end
 
   describe "information cannot be left blank" do
+    before (:each) do
+      @user = build(:user)
+    end
+
     it "is invalid without a name" do
-      user = User.new(name: nil)
-      user.valid?
-      expect(user.errors[:name]).to include("can't be blank")
+      @user.name = nil
+      @user.valid?
+      expect(@user.errors[:name]).to include("can't be blank")
     end
     it "is invalid without an email address" do
-      user = User.new(email: nil)
-      user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      @user.name = nil
+      @user.valid?
+      expect(@user.errors[:email]).to include("can't be blank")
+    end
+    it "is invalid without a password" do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors[:password]).to include("can't be blank")
     end
   end
 
   describe "information cannot already be in use" do
+    
     it "is invalid with a duplicate email address" do
       User.create(name: 'test', email: 'test@example.com', password: 'password')
       user = User.new(name: 'test', email: 'test@example.com', password: 'password')
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,9 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  # Include Factory Girl syntax to simplify calls to factories
+  config.include FactoryGirl::Syntax::Methods
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
