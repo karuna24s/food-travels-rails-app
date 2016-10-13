7fc9daeb6d12493e2fccf4332c7e19a8f3044875
karuna24s <karuna24s@gmail.com>
2016-10-13 19:42:49 -0400

    Fixed a typo in the omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    def facebook
-      @user = User.from_omniauth(request.env["omniauth.auth"])
-      sign_in_and_redirect @user
-    end
-  end
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    sign_in_and_redirect @user
+  end  
 end
