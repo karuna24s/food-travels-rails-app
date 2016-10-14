f850a9f1c207e7eb0279423eecb013ebd8badb10
karuna24s <karuna24s@gmail.com>
2016-10-14 19:20:34 -0400

    generated comments controller, add comments resource in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :destinations
   resources :foods
+  resources :comments
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
