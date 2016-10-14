2ed6b57160ea125595a396c5e75931889e78b06c
karuna24s <karuna24s@gmail.com>
2016-10-14 18:51:29 -0400

    add configure_permitted_parameters action in application controller and add name attribute in registration edit form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || root_path
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+
 end
