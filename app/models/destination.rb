014afb3580f7ca578ac6b8928f1542f185a7ac05
karuna24s <karuna24s@gmail.com>
2016-10-13 20:22:29 -0400

    removed destination model food_id, user_id validation

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -3,6 +3,4 @@ class Destination < ApplicationRecord
   belongs_to :user
   has_many :comments
   validates_presence_of :title, :content, :location
-  validates :food_id, length: { minimum: 1 }, allow_nil: true
-  validates :user_id, length: { minimum: 1 }, allow_nil: true
 end
