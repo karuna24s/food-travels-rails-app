ee4b5d028d8ac71b8cc2808ce9dcb739a9c71804
karuna24s <karuna24s@gmail.com>
2016-10-14 21:39:49 -0400

    built destination form partial, new and create controller actions, and params sanitizer

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,9 +1,24 @@
 class DestinationsController < ApplicationController
   def index
-    @destinations = Destinations.all
+    @destinations = Destination.all
+  end
+
+  def new
+    @destination = Destination.new
+  end
+
+  def create
+    @destination = Destination.create(destination_params)
+    redirect_to destination_path(@destination)
   end
 
   def show
+  end
+
+  private
+
+  def destination_params
+    params.require(:destination).permit(:title, :location, :content, :recommendation, :food_id, :user_id)
   end
 
 end
--- db/migrate/20161012170026_create_destinations.rb
+++ db/migrate/20161012170026_create_destinations.rb
@@ -2,8 +2,8 @@ class CreateDestinations < ActiveRecord::Migration[5.0]
   def change
     create_table :destinations do |t|
       t.string :title
-      t.text :content
       t.string :location
+      t.text :content
       t.string :recommendation
       t.integer :food_id
       t.integer :user_id
