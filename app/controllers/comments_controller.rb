5bd7b155b1453171b2cb7dcb3d1c81b7e874580d
karuna24s <karuna24s@gmail.com>
2016-10-15 19:03:01 -0400

    started working on the comments controller and views, also updated the show action in destination

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to comment.destination
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:name])
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :destination
   belongs_to :user
   validates :content, presence: true
+
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 end
