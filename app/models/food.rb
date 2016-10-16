37278deff05092e2ba77f5ede58916654d51a2f2
karuna24s <karuna24s@gmail.com>
2016-10-16 17:41:17 -0400

    Updated the layout of the food partial view

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,7 +6,6 @@ class Food < ApplicationRecord
   # accepts_nested_attributes_for :destinations
   def destinations_attributes=(destinations_attributes)
     destinations_attributes.values.each do |destination_attributes|
-      raise params.inspect
       self.reviews.build(destination_attributes)
     end
   end
