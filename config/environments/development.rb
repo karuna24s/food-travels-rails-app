2e57aba4e803d227a9351f0871b8a71d7e3bc164
karuna24s <karuna24s@gmail.com>
2016-10-18 16:23:43 -0400

    created a button to update the food travel story, still determining how to resolve the comments issue (make sure the comments appear in the destination show page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,10 +26,12 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
+  # # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  #
   config.action_mailer.perform_caching = false
+  # For Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -52,6 +54,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # For Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :users, only: :show
+  resources :users, only: [:show]
   resources :destinations do
     resources :foods
   end
