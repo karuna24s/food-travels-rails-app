37b02bee31f33fc68fa0c49df548687fafdd2b81
karuna24s <karuna24s@gmail.com>
2016-10-26 11:30:49 -0400

    removed commented code in the food model and the application view in the layouts food directory.

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,10 +3,4 @@ class Food < ApplicationRecord
   has_many :users, through: :destinations
   validates :name, presence: true
 
-  # # accepts_nested_attributes_for :destinations
-  # def destinations_attributes=(destinations_attributes)
-  #   destinations_attributes.each do |destination_attributes|
-  #     self.destinations.build(destination_attributes)
-  #   end
-  # end
 end
