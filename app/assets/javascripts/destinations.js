ae3613355edcf5e840bc5f6a8523032223861097
karuna24s <karuna24s@gmail.com>
2016-12-07 11:20:29 -0500

    after creating destinations.js file tried to implement destination constructor function and read more link, also required destinations.js in application.js

---
+++ app/assets/javascripts/destinations.js
@@ -0,0 +1,18 @@
+function Destination(data) {
+  this.id = data.id;
+  this.title = data.title;
+  this.location = data.location;
+  this.content = data.content;
+  this.recommendation = data.recommendation;
+  this.user = data.user;
+}
+
+$(function () {
+  $(".js-more").on('click', function() {
+    var id = $(this).data("id");
+    $.get("/destinations/" + id + "/destination_data", function(data) {
+
+      $("#content-" + id).text(data["content"]);
+    });
+  });
+});
